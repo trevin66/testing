f8336c482888400d1c15327c4e6fe9e65fac186c
John Laws Harrison <johnlawsharrison@gmail.com>
2016-07-15 22:05:41 -0700

    Add password validation

--- js/app.js
+++ js/app.js
@@ -3,4 +3,9 @@ var app = angular.module("TestApp", []);
 
 //main controller for form
 app.controller('FormCtrl', ['$scope', function($scope) {
+
+	//validate matching passwords
+	$scope.validatePassword = function (myForm) {
+		myForm.password.$setValidity('passwordsMatch', $scope.password === $scope.confirm_password);
+	}
 }]);
